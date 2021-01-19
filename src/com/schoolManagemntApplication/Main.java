5106629a48a9f36f2eab600e34993f1b09fdf672
Bbereket1 <berekettsegaye13@gmail.com>
2021-01-18 19:50:56 -0800

    add home page structure

--- src/com/schoolManagemntApplication/Main.java
+++ src/com/schoolManagemntApplication/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Chat.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         primaryStage.setTitle("School Management Application");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
--- src/com/schoolManagemntApplication/StudentController.java
+++
@@ -1,7 +0,0 @@
-package com.schoolManagemntApplication;
-
-import javafx.scene.layout.AnchorPane;
-
-public class StudentController {
-    public AnchorPane studentAnchorPane;
-}
